1fa66224c26b0fda6dd505ef04513363ccc535ce
Gerardo Lankenau <glankenau@hey.com>
2024-01-17 18:46:38 +0100

    Add views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :surveys, only: %i[show]
+  resources :questions, only: %i[show]
+  resources :answers, only: %i[create index]
+  resources :reports do
+  end
+
+  get "thanks" => "thanks#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
