c7ba0be8ae908de68a0a2971c22b8b97420d4acf
Gerardo Lankenau <glankenau@hey.com>
2024-01-22 16:26:48 +0100

    Remove headers from survey

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  def hide_header?
+    # List the routes where you want to hide the footer
+
+    return false if request.path == root_path
+    hide_header_routes = ['thanks', 'surveys', 'questions']
+
+    # Check if the current route matches any in the list
+    hide_header_routes.none? { |route| request.path.starts_with?("#{route}") }
+  end
+
+  def hide_footer?
+    return false if request.path == root_path
+    # List the routes where you want to hide the footer
+
+    hide_footer_routes = ['thanks', 'surveys*', 'questions*']
+
+    # Check if the current route matches any in the list
+    hide_footer_routes.none? { |route| request.path.starts_with?("#{route}") }
+  end
 end
